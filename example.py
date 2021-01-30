1a234e1e523eed7858b363ed381328063909d55e
TianweiCao <78243340+TianweiCao@users.noreply.github.com>
2021-01-30 17:46:01 +0800

    Create example.py

---
+++ example.py
@@ -0,0 +1,22 @@
+import numpy as np
+# ============================================================
+# Defining your own functions here
+# ============================================================
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+# ============================================================
+# Defining your own testing here
+# ============================================================
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add('boston', 'university') == 'bostonuniversity'
+
+
+def test_subtract():
+    assert subtract(2, 3) == -1
